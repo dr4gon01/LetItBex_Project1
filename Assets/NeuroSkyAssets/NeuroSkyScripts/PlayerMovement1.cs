f7aab3476810c92d3335ac1b6b57423f2df63c34
nirmeshg <nirmesh@performixbiz.com>
2016-08-28 18:51:27 -0500

    Added the player interaction according to Medidation level

---
+++ Assets/NeuroSkyAssets/NeuroSkyScripts/PlayerMovement1.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement1 : MonoBehaviour{
+
+	private Vector3 _startPosition;
+	ThinkGearController controller;
+	private int Attention;
+	public int Meditation;
+	private bool noAttention;
+	private bool isTouch;
+	AudioSource audi;
+	public GameObject enemy;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		isTouch = false;
+
+		_startPosition = this.transform.position;
+		controller = GameObject.Find ("ThinkGear").GetComponent<ThinkGearController> ();
+		controller.UpdateAttentionEvent += OnUpdateAttention;
+		controller.UpdateMeditationEvent += OnUpdateMeditation;
+		noAttention = false;
+		audi = GetComponent <AudioSource> ();
+	}
+
+	void OnUpdateAttention(int value){
+		Attention = value;
+	}
+	void OnUpdateMeditation(int value){
+		Meditation = value;
+	}
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//Vector3 pos_Forward = new Vector3 (0,0,-transform.position.z+movSpeed);
+		if (Attention > 40 && audi.isPlaying == false && isTouch == false) {
+			transform.position += Vector3.forward * Attention * Time.deltaTime;
+			audi.volume = Random.Range (0.8f, 1f);
+			audi.pitch = Random.Range (0.8f, 1.2f);
+			audi.Play ();
+		} 
+		if (isTouch == true && Meditation > 50) {
+			Destroy (enemy);
+		}
+			
+
+
+}
+	void OnTriggerEnter(Collider other) {
+		//1. on trigger stop the player & start considering the meditation level
+		
+
+		if (other.tag == "enemy" ) {
+			isTouch = true;
+			}
+	}
+		
+
+}
